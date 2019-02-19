e202c1e0fcbea14eb4604bf365c241b867b1c5a2
Noah <noah@noah.email>
2019-02-19 15:03:05 -0500

    second commit of usa.py

--- usa.py
+++ usa.py
@@ -17,9 +17,34 @@ def beginUSA():
 		firstChoice = raw_input()
 		if "1" in firstChoice:
 			print "The most you can move the date up is to is April 30th, 5 days before what was scheduled."
+			time.sleep(4);
+			earlyDate();
 		elif "2" in firstChoice:
 			print "It would be too dangerous to move the date up sooner"
+			time.sleep(4);
+			normalDate();
 		else:
         	       	print "Type either 1 or 2"
   	        	continue
 	return
+
+
+def earlyDate():
+	print "It is strongly suggested that you do not do this as you could put the mission in danger."
+	time.sleep(4)
+	print "Would you still like to continue? (yes/no)"
+	
+	while True: 
+		sureDecision = raw_input()
+		if "yes" in sureDecision:
+			print "OK. You're the boss. April 30th it is."
+		elif "no" in sureDecision:
+			print "I think that was the right decision"
+		else:
+			print "Type yes or no"
+			
+
+
+def normalDate():
+	print "There is still a lot to be done before the launch date."
+	print "Everything will proceed as planned"
